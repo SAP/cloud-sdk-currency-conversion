3683b8eb98354a9ff290ac6157fbb54aef634607
Suhas Rao <suhas.rao.kallianpur@sap.com>
2021-02-10 15:37:46 +0530

    Fixed lint issues

--- packages/models/test/exchange-rate-value.test.ts
+++ packages/models/test/exchange-rate-value.test.ts
@@ -1,5 +1,4 @@
 /* Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. */
-import { BigNumber } from 'bignumber.js';
 import { ConversionModelError } from '../src/constants/conversion-model-error';
 import { ExchangeRateValue } from '../src/exchange-rate-value';
 
